c1798ef6b7d1450fe98b12551e53a4a4b0ae502e
hatch5o6 <hatch5o6@byu.edu>
2026-04-28 10:48:06 -0600

    added clean_slurm_outputs script

---
+++ Pipeline/clean_slurm_outputs.py
@@ -0,0 +1,39 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--top", "-t", type=int, default=1)
+args = parser.parse_args()
+top = args.top
+
+
+dirs = [
+    "Pipeline/slurm_outputs/slurm_outputs", 
+    "Pipeline/slurm_outputs/test_slurm_outputs",
+    "Pipeline/slurm_outputs/hyper_param_search_outputs_oc_abl/",
+    "Pipeline/slurm_outputs/hyper_param_search_outputs",
+    "Pipeline/slurm_outputs/SC_smt",
+    "Pipeline/slurm_outputs/SC",
+    "Pipeline/slurm_outputs/predict"
+]
+for dir in dirs:
+    if not os.path.exists(dir): continue
+    fs = os.listdir(dir)
+    files = {}
+    for f in fs:
+        if f.endswith(".py"): continue
+        if f.startswith("SAVE_"): continue
+        number = f.split("_")[0]
+        number = int(number)
+        name = "_".join(f.split("_")[1:])
+        if name not in files:
+            files[name] = []
+        
+        files[name].append((number, f))
+
+    for name, fs in files.items():
+        fs.sort(reverse=True)
+
+        for n, f in fs[top:]:
+            f_path = os.path.join(dir, f)
+            os.remove(f_path)
